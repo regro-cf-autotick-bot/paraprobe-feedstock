4fe4415bb6460a52db03307816feb389cc2c7c04
Sarath Menon <sarathmenon@mailbox.org>
2023-09-12 16:30:00 +0200

    update patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -47,64 +47,64 @@ make
 cp paraprobe_tessellator ${PREFIX}/bin/
 cd ..
 
-cd paraprobe-distancer
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_distancer ${PREFIX}/bin/
-cd ..
+#cd paraprobe-distancer
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_distancer ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-intersector
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_intersector ${PREFIX}/bin/
-cd ..
+#cd paraprobe-intersector
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_intersector ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-nanochem
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_nanochem ${PREFIX}/bin/
-cd ..
+#cd paraprobe-nanochem
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_nanochem ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-ranger
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_ranger ${PREFIX}/bin/
-cd ..
+#cd paraprobe-ranger
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_ranger ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-spatstat
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_spatstat ${PREFIX}/bin/
-cd ..
+#cd paraprobe-spatstat
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_spatstat ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-surfacer
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_surfacer ${PREFIX}/bin/
-cd ..
+#cd paraprobe-surfacer
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_surfacer ${PREFIX}/bin/
+#cd ..
 
 mv $mpifort.orig $mpifort
